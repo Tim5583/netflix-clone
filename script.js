301050140a078a5d37b94b9e2085e8c1dc23ee6d
T.Praveen <90211663+Tim5583@users.noreply.github.com>
2022-03-15 16:34:50 +0530

    update active element

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const accordion = document.querySelectorAll(".accordion-button");
+const activeEl = document.querySelector(".active");
 
 for (let i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", function() {
 
-        if (document.querySelector(".active") && document.querySelector(".active") != this.nextElementSibling) {
-            document.querySelector(".active").classList.remove("active");
+        if (activeEl && activeEl != this.nextElementSibling) {
+            activeEl.classList.remove("active");
             document.querySelector(".icon-ro").classList.remove("icon-ro")
         }
 
